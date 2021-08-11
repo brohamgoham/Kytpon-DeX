5d4054ffaa9c4a2f10bee2c0dccab79f628cf13c
brohamgoham <mohamgoham@gmail.com>
2021-08-11 00:55:05 -0400

    master

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,210 @@
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+import { withStyles } from '@material-ui/core/styles';
+import {
+  Typography,
+} from '@material-ui/core';
+import { colors } from '../../theme'
+
+import AttachMoneyIcon from '@material-ui/icons/AttachMoney';
+import HowToVoteIcon from '@material-ui/icons/HowToVote';
+import DescriptionIcon from '@material-ui/icons/Description';
+import MonetizationOnIcon from '@material-ui/icons/MonetizationOn';
+
+import ForumIcon from '@material-ui/icons/Forum';
+import BarChartIcon from '@material-ui/icons/BarChart';
+import BuildIcon from '@material-ui/icons/Build';
+
+import BuiltWithModal from '../builtwith/builtwithModal.jsx'
+
+const styles = theme => ({
+  footer: {
+    padding: '24px',
+    display: 'flex',
+    justifyContent: 'space-evenly',
+    width: '100%',
+    background: colors.white,
+    borderRadius: '50px 50px 0px 0px',
+    border: '1px solid '+colors.borderBlue,
+    borderBottom: 'none',
+    marginTop: '48px',
+    flexWrap: 'wrap',
+    [theme.breakpoints.down('xs')]: {
+      justifyContent: 'flex-start',
+    }
+  },
+  heading: {
+    marginBottom: '12px',
+    paddingBottom: '9px',
+    borderBottom: "3px solid "+colors.borderBlue,
+    width: 'fit-content',
+    marginLeft: '30px'
+  },
+  link: {
+    paddingBottom: '12px',
+    cursor: 'pointer',
+    display: 'flex',
+    alignItems: 'center',
+    '&:hover': {
+      textDecoration: 'underline'
+    }
+  },
+  icon: {
+    fill: colors.borderBlue,
+    marginRight: '6px'
+  },
+  KryptonIcon: {
+    minHeight: '100%',
+    display: 'flex',
+    alignItems: 'center'
+  },
+  builtWith:{
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    [theme.breakpoints.down('md')]: {
+      display: 'none',
+    }
+  },
+  builtWithLink: {
+    paddingTop: '12px'
+  },
+  builtHeading: {
+    marginBottom: '12px',
+    paddingBottom: '9px',
+    borderBottom: "3px solid "+colors.borderBlue,
+    width: 'fit-content',
+  },
+  products: {
+    padding: '0px 24px',
+    [theme.breakpoints.down('xs')]: {
+      paddingBottom: '24px'
+    }
+  },
+  community: {
+    padding: '0px 24px',
+    [theme.breakpoints.down('xs')]: {
+      paddingBottom: '24px'
+    }
+  },
+  socials: {
+    padding: '0px 24px'
+  }
+});
+
+
+class Footer extends Component {
+
+  constructor(props) {
+    super()
+
+    this.state = {
+      modalBuiltWithOpen: false,
+    }
+  }
+
+  render() {
+    const { classes, location } = this.props;
+    const {
+      modalBuiltWithOpen
+    } = this.state
+
+    if(location.pathname === '' || location.pathname === '/') {
+      return null
+    }
+
+    return (
+      <div className={classes.footer}>
+        <div className={ classes.builtWith }>
+          <Typography className={ classes.builtHeading } variant={ 'h6'}>Krypton Finance</Typography>
+          <img
+            alt=""
+            src={ require('../../assets/YFI-logo.png') }
+            height={ '120px' }
+            width={ '120px' }
+          />
+          <div  className={ `${classes.link} ${classes.builtWithLink}` } onClick={ () => { this.builtWithOverlayClicked() } } >
+            <BuildIcon height='15px' className={ classes.icon } />
+            <Typography variant={ 'h4'} >Built with</Typography>
+          </div>
+        </div>
+        <div className={ classes.products }>
+          <Typography className={ classes.heading } variant={ 'h6'}>Products</Typography>
+          <div  className={ classes.link } onClick={()=> window.open("https://krypton.io", "_blank")} >
+            <AttachMoneyIcon height='15px' className={ classes.icon } />
+            <Typography variant={ 'h4'} >Krypton Finance</Typography>
+          </div>
+          <div  className={ classes.link } onClick={()=> window.open("https://krypton.io", "_blank")} >
+            <HowToVoteIcon height='15px' className={ classes.icon } />
+            <Typography variant={ 'h4'} >Krypton Governance</Typography>
+          </div>
+          <div  className={ classes.link } onClick={()=> window.open("https://krypton.io", "_blank")} >
+            <MonetizationOnIcon height='15px' className={ classes.icon } />
+            <Typography variant={ 'h4'} >Krypton's DeFi and Crypto Insurance</Typography>
+          </div>
+          <div  className={ classes.link } onClick={()=> window.open("https://krypton.io", "_blank")} >
+            <DescriptionIcon height='15px' className={ classes.icon } />
+            <Typography variant={ 'h4'} >Krypton Platform Doc's</Typography>
+          </div>
+        </div>
+        <div className={ classes.community }>
+          <Typography className={ classes.heading } variant={ 'h6'}>Community</Typography>
+          <div  className={ classes.link } onClick={()=> window.open("https://krypton.io", "_blank")} >
+            <DescriptionIcon height='15px' className={ classes.icon } />
+            <Typography variant={ 'h4'} >Binance</Typography>
+          </div>
+          <div  className={ classes.link } onClick={()=> window.open("https://krypton.io", "_blank")} >
+            <BarChartIcon height='15px' className={ classes.icon } />
+            <Typography variant={ 'h4'} >Ethereum</Typography>
+          </div>
+          <div  className={ classes.link } onClick={()=> window.open("hhttps://krypton.io", "_blank")} >
+            <BarChartIcon height='15px' className={ classes.icon } />
+            <Typography variant={ 'h4'} >Conflux</Typography>
+          </div>
+          <div  className={ classes.link } onClick={()=> window.open("https://krypton.io", "_blank")} >
+            <BarChartIcon height='15px' className={ classes.icon } />
+            <Typography variant={ 'h4'} >Cosmos Hub</Typography>
+          </div>
+          <div  className={ classes.link } onClick={()=> window.open("https://krypton.io", "_blank")} >
+            <ForumIcon height='15px' className={ classes.icon } />
+            <Typography variant={ 'h4'} >Contact Us</Typography>
+          </div>
+          <div  className={ classes.link } onClick={()=> window.open("https://krypton.io", "_blank")} >
+            <AttachMoneyIcon height='15px' className={ classes.icon } />
+            <Typography variant={ 'h4'} >Fortress of Solitude Finance</Typography>
+          </div>
+        </div>
+        <div className={ classes.socials }>
+          <Typography className={ classes.heading } variant={ 'h6'}>Resources</Typography>
+          <div  className={ classes.link } onClick={()=> window.open("https://twitter.com/mohamgoham", "_blank")} >
+            <img alt="" src={ require('../../assets/twitter.svg') } height='24px' className={ classes.icon } />
+            <Typography variant={ 'h4'} >Twitter</Typography>
+          </div>
+       
+          <div  className={ classes.link } onClick={()=> window.open("https://github.com/brohamgoham", "_blank")} >
+            <img alt="" src={ require('../../assets/github.svg') } height='24px' className={ classes.icon } />
+            <Typography variant={ 'h4'} >Github</Typography>
+          </div>
+          
+        </div>
+        { modalBuiltWithOpen && this.renderBuiltWithModal() }
+      </div>
+    )
+  }
+
+  renderBuiltWithModal = () => {
+    return (
+      <BuiltWithModal closeModal={ this.closeBuiltWithModal } modalOpen={ this.state.modalBuiltWithOpen } />
+    )
+  }
+
+  builtWithOverlayClicked = () => {
+    this.setState({ modalBuiltWithOpen: true })
+  }
+
+  closeBuiltWithModal = () => {
+    this.setState({ modalBuiltWithOpen: false })
+  }
+}
+
+export default withRouter(withStyles(styles)(Footer));
